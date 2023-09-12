1ec585712e40032fc2bc92db2d300b916792b956
Priyank Verma <prynq@Priyanks-MacBook-Air.local>
2023-09-12 17:22:25 +0530

    coming soon ui fix

--- app/components/comingSoon/index.js
+++ app/components/comingSoon/index.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 export default function ComingSoon() {
     return (
-        <div className="min-h-screen flex flex-col justify-center font-extrabold text-xl text-center text-night items-center px-10 coming__soon">
+        <div className="min-h-screen w-screen flex flex-col justify-center font-extrabold whitespace-normal text-xl text-center text-night items-center px-10 coming__soon">
             <h1>This UI is still cooking, meanwhile open on a bigger screen</h1>
             <Image
                 className="absolute bottom-0"
